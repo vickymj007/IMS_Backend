9c97424b1b114c3361cbe4664662bc17747971cd
Vicky <vickypersish1997@gmail.com>
2024-04-13 15:26:31 +0530

    Added new testing route to verify IP Rotation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
-const userController = require('../controllers/userController')
-const router = require('express').Router()
+const userController = require("../controllers/userController");
+const router = require("express").Router();
 
+router.post("/user", userController.addUser);
+router.get("/user", userController.getAllUser);
+router.get("/user/:id", userController.getUser);
+router.put("/user/:id", userController.updateUser);
+router.delete("/user/:id", userController.deleteUser);
+router.get("/verify-ip", async (req, res) => {
+  try {
+    res.status(200).json(req);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-router.post('/user',userController.addUser)
-router.get('/user',userController.getAllUser)
-router.get('/user/:id',userController.getUser)
-router.put('/user/:id',userController.updateUser)
-router.delete('/user/:id',userController.deleteUser)
-
-
-module.exports = router
+module.exports = router;
